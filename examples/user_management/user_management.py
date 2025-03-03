f7661123e057fee47e3d07f5698354181e70c486
Python Russ 2 <russ@omni.co>
2025-03-02 21:15:03 -0800

    additional methods and usage examples

---
+++ examples/user_management/user_management.py
@@ -0,0 +1,25 @@
+import OmniAPI
+import csv, time
+
+api_key = '<<your api key>>'
+base_url = 'https://<<your omni host>>/api'
+
+# Initialize the API with your credentials
+api = OmniAPI(api_key, base_url)
+
+
+with open('users.csv', newline='') as csvfile:
+    spamreader = csv.DictReader(csvfile)
+    for row in spamreader:
+        time.sleep(2)
+        email = row.pop('email')
+        displayName = row.pop('display_name')
+        op = row.pop('op')
+        if op == 'upsert':
+            r1 = api.upsert_user(
+                email=email,
+                displayName=displayName,
+                attributes=row
+            )
+        elif op == 'delete':
+            api.delete_user(email)
