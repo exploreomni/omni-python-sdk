0076b7d2173a80ba5aa480d86a794bbb9fd435e5
Russell Garner <russ@omni.co>
2025-03-03 10:50:24 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='omni_python_sdk',
-	version='0.1.0',
+	version='0.1.1',
 	description='A Python SDK for Omni API',
 	long_description=open('README.md').read(),
 	long_description_content_type='text/markdown',
